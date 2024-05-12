3b1b8b865d9d7f5f25d67df8fdc67c507f3d1981
Ben <benmega@gmail.com>
2024-05-12 14:09:22 +0700

    user.db addition and user history logic

---
+++ models.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ip_address = db.Column(db.String(15), unique=True, nullable=False)
+    username = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
---
+++ static/script.js
@@ -0,0 +1,58 @@
+    // Event listener for username form submission
+    document.getElementById('usernameForm').addEventListener('submit', function(e) {
+        e.preventDefault();
+        var username = document.getElementById('username').value;
+        document.getElementById('currentUsername').value = username;
+    });
+
+    // Event listener for message form submission
+    document.getElementById('messageForm').addEventListener('submit', function(e) {
+        e.preventDefault();
+        var message = document.getElementById('message').value;
+        var username = document.getElementById('currentUsername').value;
+
+        // Check if username is set
+//        if (!username) {
+//            alert("Please set your username first!");
+//            return;
+//        }
+        var params = new URLSearchParams();
+        params.append('message', message);
+        params.append('username', username);
+
+        fetch('/send_message', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: params
+        }).then(function(response) {
+            if (response.ok) {
+                document.getElementById('message').value = '';
+            }
+        });
+    });
+
+    // Function to update the conversation
+    function updateConversation() {
+        fetch('/get_conversation')
+            .then(response => response.json())
+            .then(data => {
+                var chatDiv = document.getElementById('chat');
+                chatDiv.innerHTML = '';
+                data.conversation_history.forEach(function(entry) {
+                    var user = entry[0];
+                    var message = entry[1];
+                    // Convert URLs to clickable links
+                    //var urlRegex = /(https?:\/\/[^\s]+)/g;
+                    var urlRegex = /(https?:\/\/[^\s]+)|(\bwww\.[^\s]+(?:\.[^\s]+)+\b)/g;
+                    message = message.replace(urlRegex, function(url) {
+                        return '<a href="' + url + '" target="_blank">' + url + '</a>';
+                    });
+                    chatDiv.innerHTML += '<p><strong>' + user + ':</strong> ' + message + '</p>';
+                });
+            });
+    }
+
+    // Set an interval to update the conversation every 2 seconds
+    setInterval(updateConversation, 2000);
