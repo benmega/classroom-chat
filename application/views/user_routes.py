60aee3b8cae32f7ad195a86f966e9bde135c4cdd
Ben <benmega@gmail.com>
2024-05-19 09:59:24 +0700

    Clean and Test

--- application/views/user_routes.py
+++ application/views/user_routes.py
@@ -8,20 +8,49 @@ from application.models.user import User, db
 user_bp = Blueprint('user_bp', __name__)
 conversation_history = []
 
+
 @user_bp.route('/send_message', methods=['POST'])
 def send_message():
     # TODO refactor to part of the user object
     # TODO make user object
     user_ip = request.remote_addr
-    username = request.form['username']
+    formUsername = request.form['username']
+    user_message = request.form['message']
+    user = get_or_make_user(user_ip, formUsername)
+    conversation_history.append((user.username, user_message))
+
+    if not ChatBotEnabled:
+        return jsonify(success=True)
+
+    return jsonify(success=True, ai_response=get_ai_response(user_message, user.username))
+
+
+@user_bp.route('/get_users', methods=['GET'])
+def get_users():
+    pass
+    # Logic to retrieve users
+
+
+@user_bp.route('/admin/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    pass
+    # Logic to update user
+
+
+@user_bp.route('/get_conversation', methods=['GET'])
+def get_conversation():
+    return jsonify(conversation_history=conversation_history)
+
+
+def get_or_make_user(user_ip, formUsername=""):
+    # retrieve user info or create a new user with given username and ip
     user = User.query.filter_by(ip_address=user_ip).first()
-    print(f'sending message from {user_ip} ({username})')
+    print(f'sending message from {user_ip} ({formUsername})')
     if user:
-        if not username:
-            username = user.username
-        elif user.username != username:
-            print(f"Updating user from {user.username} to {username}")
-            user.username = username
+        # return user
+        if user.username != formUsername:
+            print(f"Updating user from {user.username} to {formUsername}")
+            user.username = formUsername
             try:
                 db.session.commit()
             except Exception as e:
@@ -29,28 +58,7 @@ def send_message():
                 db.session.rollback()  # Roll back on error
     else:
         print("No user found, creating a new one.")
-        user = User(ip_address=user_ip, username=username)
+        user = User(ip_address=user_ip, username=formUsername)
         db.session.add(user)
         db.session.commit()
-
-    user_message = request.form['message']
-    conversation_history.append((username, user_message))
-
-    if not ChatBotEnabled:
-        return jsonify(success=True)
-
-    return jsonify(success=True, ai_response=get_ai_response(user_message, username))
-
-@user_bp.route('/get_users', methods=['GET'])
-def get_users():
-    pass
-    # Logic to retrieve users
-
-@user_bp.route('/admin/users/<int:user_id>', methods=['PUT'])
-def update_user(user_id):
-    pass
-    # Logic to update user
-
-@user_bp.route('/get_conversation', methods=['GET'])
-def get_conversation():
-    return jsonify(conversation_history=conversation_history)
+    return user
