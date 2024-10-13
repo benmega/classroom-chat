299becd61cf2e4f34133e9b71c6dfbad31aa4855
Ben <benmega@gmail.com>
2024-10-13 16:33:14 +0700

    cleanup

--- application/routes/message_routes.py
+++ application/routes/message_routes.py
@@ -48,40 +48,6 @@ def send_message():
     return jsonify(success=True), 200
 
 
-# @message_bp.route('/send_message', methods=['POST'])
-# def send_message():
-#     user_ip = request.remote_addr
-#     form_username = request.form['username']
-#     form_message = request.form['message']
-#
-#     # Handle user creation
-#     user = get_or_make_user(user_ip, form_username)
-#     if not user:
-#         return jsonify(success=False, error="Unknown User"), 500
-#
-#     # Handle configuration check
-#     config = Configuration.query.first()
-#     if not config:
-#         return jsonify(success=False, error="No Configuration Found"), 500
-#     if user.username != adminUsername and not config.message_sending_enabled:
-#         return jsonify(success=False, error="Non-admin messages are disabled"), 500
-#
-#     # Check for banned words
-#     if not message_is_appropriate(form_message):
-#         return jsonify(success=False, error="Inappropriate messages are not allowed"), 500
-#
-#     # Save message to the database
-#     if not save_message_to_db(user.id, form_message):
-#         return jsonify(success=False, error="Database commit failed"), 500
-#
-#     # Get AI response if Chatbot is enabled
-#     if config.ai_teacher_enabled:
-#         ai_response = get_ai_response(form_message, user.username)
-#         return jsonify(success=True, ai_response=ai_response)
-#
-#     return jsonify(success=True), 200
-
-
 def save_message_to_db(user_id, message):
     conversation = Conversation(message=message, user_id=user_id)
     db.session.add(conversation)
