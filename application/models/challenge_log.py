60bbcae80d75d178510ba93281bee61a18b26bc4
Ben <benmega@gmail.com>
2024-12-14 21:25:44 +0700

    debug

--- application/models/challenge_log.py
+++ application/models/challenge_log.py
@@ -10,6 +10,7 @@ class ChallengeLog(db.Model):
     course_id = db.Column(db.String(100), nullable=True)  # Optional for challenges without courses
     course_instance = db.Column(db.String(100), nullable=True)  # Optional for flexibility
     timestamp = db.Column(db.DateTime, nullable=False, default=db.func.now())  # Auto-populates with current time
+    # challenge = db.relationship('Challenge', backref='logs', lazy=True)
 
     def __repr__(self):
         return f"<ChallengeLog(username={self.username}, domain={self.domain}, challenge={self.challenge_name}, timestamp={self.timestamp})>"
