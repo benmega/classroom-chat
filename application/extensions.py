038ecc318f8fd0b9a8008b28f4e60ae00be8a4b1
Ben <benmega@gmail.com>
2025-04-09 19:49:51 +0700

    feature: message limiter using flask limiter

--- application/extensions.py
+++ application/extensions.py
@@ -1,6 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
+limiter = Limiter(key_func=get_remote_address)
 db = SQLAlchemy()
 
 # Example with threading, which does not require additional installations
