a8cf5d755f58b1a4cd5272563f24c96f56beed93
Ben <benmega@gmail.com>
2025-04-15 09:25:58 +0700

    chore: stop tracking dist folder

--- dist/main/_internal/application/models/configuration.py
+++
@@ -1,7 +0,0 @@
-from application.extensions import db
-
-
-class Configuration(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    ai_teacher_enabled = db.Column(db.Boolean, default=False)
-    message_sending_enabled = db.Column(db.Boolean, default=False)
