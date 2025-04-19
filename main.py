6200e0ab080059d870ca90d3cd100471b383d902
Ben <benmega@gmail.com>
2025-04-19 09:40:15 +0700

    refactor: minor adjustments

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from application import create_app
 from application.extensions import socketio
 
@@ -8,7 +10,7 @@ def main():
                  host='0.0.0.0',  # previously 0.0.0.0 allows the server to be accessible network-wide
                  port=5000,  # previously 5000. TODO altert students!! Sets the port the application will listen on
                  log_output=True,  # Enables or disables the logging output by the server
-                 use_reloader=True,  # Enable or disable the reloader
+                 use_reloader=not getattr(sys, 'frozen', False),  # Enable or disable the reloader
                  allow_unsafe_werkzeug=True,
                  debug=False)  # Toggle debug mode for Flask and SocketIO
 
