f4dbc9912f940e40135c49fd99a9bb8dd98ca0b2
Ben <benmega@gmail.com>
2025-09-02 16:01:59 +0700

    feature: source field added to achievements

--- instance/utilities/add_achievements.py
+++ instance/utilities/add_achievements.py
@@ -12,30 +12,38 @@ def seed_achievements():
     """Insert predefined achievements if they do not already exist."""
 
     achievements = [
-        {"slug": "560f1a9f22961295f9427742", "name": "CS1", "description": "Submit completion certificate for CS1",
-         "type": "custom", "requirement_value": "1"},
-        {"slug": "5632661322961295f9428638", "name": "CS2", "description": "Submit completion certificate for CS2",
-         "type": "custom", "requirement_value": "1"},
-        {"slug": "56462f935afde0c6fd30fc8c", "name": "CS3", "description": "Submit completion certificate for CS3",
-         "type": "custom", "requirement_value": "1"},
-        {"slug": "56462f935afde0c6fd30fc8d", "name": "CS4", "description": "Submit completion certificate for CS4",
-         "type": "custom", "requirement_value": "1"},
-        {"slug": "569ed916efa72b0ced971447", "name": "CS5", "description": "Submit completion certificate for CS5",
-         "type": "custom", "requirement_value": "1"},
-        {"slug": "5817d673e85d1220db624ca4", "name": "CS6", "description": "Submit completion certificate for CS6",
-         "type": "custom", "requirement_value": "1"},
-        {"slug": "5789587aad86a6efb573701e", "name": "GD1", "description": "Submit completion certificate for GD1",
-         "type": "custom", "requirement_value": "1"},
-        {"slug": "57b621e7ad86a6efb5737e64", "name": "GD2", "description": "Submit completion certificate for GD2",
-         "type": "custom", "requirement_value": "1"},
-        {"slug": "5a0df02b8f2391437740f74f", "name": "GD3", "description": "Submit completion certificate for GD3",
-         "type": "custom", "requirement_value": "1"},
-        {"slug": "65f32b6c87c07dbeb5ba1936", "name": "Junior",
-         "description": "Submit completion certificate for Junior", "type": "custom", "requirement_value": "1"},
-        {"slug": "5789587aad86a6efb573701f", "name": "WD1", "description": "Submit completion certificate for WD1",
-         "type": "custom", "requirement_value": "1"},
-        {"slug": "5789587aad86a6efb5737020", "name": "WD2", "description": "Submit completion certificate for WD2",
-         "type": "custom", "requirement_value": "1"},
+        {"slug": "codecombat-first-level", "name": "Novice Coder", "description": "Complete your first level",
+         "type": "progress", "requirement_value": "1", "source": "www.codecombat.com", "reward": 1},
+
+        {"slug": "codecombat-5-levels", "name": "Apprentice Adventurer", "description": "Complete 5 levels",
+         "type": "progress", "requirement_value": "5", "source": "www.codecombat.com", "reward": 2},
+
+        {"slug": "codecombat-10-levels", "name": "Coding Recruit", "description": "Complete 10 levels",
+         "type": "progress", "requirement_value": "10", "source": "www.codecombat.com", "reward": 4},
+
+        {"slug": "codecombat-25-levels", "name": "Script Squire", "description": "Complete 25 levels",
+         "type": "progress", "requirement_value": "25", "source": "www.codecombat.com", "reward": 8},
+
+        {"slug": "codecombat-50-levels", "name": "Algorithm Adept", "description": "Complete 50 levels",
+         "type": "progress", "requirement_value": "50", "source": "www.codecombat.com", "reward": 15},
+
+        {"slug": "codecombat-100-levels", "name": "Code Knight", "description": "Complete 100 levels",
+         "type": "progress", "requirement_value": "100", "source": "www.codecombat.com", "reward": 30},
+
+        {"slug": "codecombat-200-levels", "name": "Master Programmer", "description": "Complete 200 levels",
+         "type": "progress", "requirement_value": "200", "source": "www.codecombat.com", "reward": 60},
+
+        {"slug": "codecombat-300-levels", "name": "Code Champion", "description": "Complete 300 levels",
+         "type": "progress", "requirement_value": "300", "source": "www.codecombat.com", "reward": 100},
+
+        {"slug": "codecombat-400-levels", "name": "Legendary Coder", "description": "Complete 400 levels",
+         "type": "progress", "requirement_value": "400", "source": "www.codecombat.com", "reward": 140},
+
+        {"slug": "codecombat-500-levels", "name": "Heroic Hacker", "description": "Complete 500 levels",
+         "type": "progress", "requirement_value": "500", "source": "www.codecombat.com", "reward": 180},
+
+        {"slug": "codecombat-570-levels", "name": "CodeCombat Conqueror", "description": "Complete all 570 levels",
+         "type": "progress", "requirement_value": "570", "source": "www.codecombat.com", "reward": 200},
     ]
 
     inserted, skipped = 0, 0
@@ -60,6 +68,33 @@ def main():
 if __name__ == "__main__":
     main()
 
+
+    # achievements = [
+    #     {"slug": "560f1a9f22961295f9427742", "name": "CS1", "description": "Submit completion certificate for CS1",
+    #      "type": "custom", "requirement_value": "1"},
+    #     {"slug": "5632661322961295f9428638", "name": "CS2", "description": "Submit completion certificate for CS2",
+    #      "type": "custom", "requirement_value": "1"},
+    #     {"slug": "56462f935afde0c6fd30fc8c", "name": "CS3", "description": "Submit completion certificate for CS3",
+    #      "type": "custom", "requirement_value": "1"},
+    #     {"slug": "56462f935afde0c6fd30fc8d", "name": "CS4", "description": "Submit completion certificate for CS4",
+    #      "type": "custom", "requirement_value": "1"},
+    #     {"slug": "569ed916efa72b0ced971447", "name": "CS5", "description": "Submit completion certificate for CS5",
+    #      "type": "custom", "requirement_value": "1"},
+    #     {"slug": "5817d673e85d1220db624ca4", "name": "CS6", "description": "Submit completion certificate for CS6",
+    #      "type": "custom", "requirement_value": "1"},
+    #     {"slug": "5789587aad86a6efb573701e", "name": "GD1", "description": "Submit completion certificate for GD1",
+    #      "type": "custom", "requirement_value": "1"},
+    #     {"slug": "57b621e7ad86a6efb5737e64", "name": "GD2", "description": "Submit completion certificate for GD2",
+    #      "type": "custom", "requirement_value": "1"},
+    #     {"slug": "5a0df02b8f2391437740f74f", "name": "GD3", "description": "Submit completion certificate for GD3",
+    #      "type": "custom", "requirement_value": "1"},
+    #     {"slug": "65f32b6c87c07dbeb5ba1936", "name": "Junior",
+    #      "description": "Submit completion certificate for Junior", "type": "custom", "requirement_value": "1"},
+    #     {"slug": "5789587aad86a6efb573701f", "name": "WD1", "description": "Submit completion certificate for WD1",
+    #      "type": "custom", "requirement_value": "1"},
+    #     {"slug": "5789587aad86a6efb5737020", "name": "WD2", "description": "Submit completion certificate for WD2",
+    #      "type": "custom", "requirement_value": "1"},
+    # ]
 
     # achievements = [
     #     # Ducks
