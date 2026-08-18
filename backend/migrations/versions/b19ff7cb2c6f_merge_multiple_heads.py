dcf636e57a1caec71caa4b32d194b135edc73935
Ben <benmega@gmail.com>
2026-08-18 22:00:33 +0700

    fix(db): merge multiple migration heads

---
+++ backend/migrations/versions/b19ff7cb2c6f_merge_multiple_heads.py
@@ -0,0 +1,23 @@
+"""merge multiple heads
+
+Revision ID: b19ff7cb2c6f
+Revises: 707930904b7d, af49041d4438
+Create Date: 2026-08-18 22:00:22.155142
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = 'b19ff7cb2c6f'
+down_revision = ('707930904b7d', 'af49041d4438')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
